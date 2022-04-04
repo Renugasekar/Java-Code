6649e5bf1cfc6d4ff04ce0a7d9fdf90e4b4660fb
Renugasekar <102687350+Renugasekar@users.noreply.github.com>
2022-04-04 10:47:12 +0530

    JAVA

---
+++ ArthmeticOp.java
@@ -0,0 +1,43 @@
+
+public class ArthmeticOp {
+
+	public static void main(String[] args) 
+	{
+		
+		int num1=6, num2=4,sum,sub,multi,div,mod;
+		System.out.println("ADDITION");
+		System.out.println("num1:"+num1);
+		System.out.println("num2:"+num2);
+		sum=num1+num2;
+		System.out.println("Addition:"+sum);
+		
+		System.out.println("");
+		System.out.println("SUBTRACTION");
+		System.out.println("num1:"+num1);
+		System.out.println("num2:"+num2);
+		sub=num1-num2;
+		System.out.println("Subtraction:"+sub);
+		
+		System.out.println("");
+		System.out.println("MULTIPLICATION");
+		System.out.println("num1:"+num1);
+		System.out.println("num2:"+num2);
+		multi=num1*num2;
+		System.out.println("Multiplication:"+multi);
+		
+		System.out.println("");
+		System.out.println("DIVISION");
+		System.out.println("num1:"+num1);
+		System.out.println("num2:"+num2);
+		div=num1/num2;
+		System.out.println("Division:"+div);
+		
+		System.out.println("");
+		System.out.println("MODULUS");
+		System.out.println("num1:"+num1);
+		System.out.println("num2:"+num2);
+		mod=num1%num2;
+		System.out.println("Reminder:"+mod);
+	}
+
+}
