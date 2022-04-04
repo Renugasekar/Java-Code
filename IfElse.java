69cbd51ff8beccca2d457f12753ba3a2ff7e3f95
Renugasekar <102687350+Renugasekar@users.noreply.github.com>
2022-04-04 10:48:32 +0530

    JAVA

---
+++ IfElse.java
@@ -0,0 +1,26 @@
+
+public class IfElse {
+
+	public static void main(String[] args)
+	{
+		int n=20;
+		
+		if(n<5)
+		{
+			System.out.println("The value of n is 5");
+		}
+		else if(n>30)
+		{
+			System.out.println("The value of n is 30");
+		}
+		else if(n>10)
+		{
+			System.out.println("The value of n is 20");
+		}	
+		else
+		{
+			System.out.println("The value of n is not 20");
+		}
+	}
+
+}
