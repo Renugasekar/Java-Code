e462fd856b84caea8dbee0c9ee9c6d1dc164bf8c
Renugasekar <102687350+Renugasekar@users.noreply.github.com>
2022-04-05 11:09:52 +0530

    JAVA

---
+++ SwitchStatement.java
@@ -0,0 +1,25 @@
+
+public class SwitchStatement 
+{
+	public static void main(String[] args)
+	{
+		int n=3;
+		
+		switch(n)
+		{
+		case 1:
+			System.out.println("The number is 1");
+		break;
+		
+		case 2:
+			System.out.println("The number is 2");
+		break;
+		
+		case 3:System.out.println("The number is 3");
+		break;
+		
+		default:System.out.println("No Match");
+		break;
+		}
+	}
+}
