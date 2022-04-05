935ab43a98999c866355da153fc97305850d5bff
Renugasekar <102687350+Renugasekar@users.noreply.github.com>
2022-04-05 11:09:19 +0530

    JAVA

---
+++ TernaryOp.java
@@ -0,0 +1,13 @@
+
+public class TernaryOp 
+{
+	public static void main(String[] args)
+	{
+		int i=5;
+		int j=10;
+		int max;
+		
+		max=(j>i)?j:i;
+		System.out.println("The Maximum value is:"+max);
+	}
+}
